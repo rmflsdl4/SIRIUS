f7d04d82393a4430518862799f7dc76fd08e5504
rmflsdl4 <rmflsdl4@gmail.com>
2024-05-07 16:56:33 +0900

    큐리소 프론트

---
+++ Resource/CULISO/front/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// import { Intro } from "./Intro";
+import { Main } from "./Main";
+import "./style.css"; // 스타일 파일 가져오기
+
+// Screen 컴포넌트를 렌더링하는 함수
+const App = () => {
+  return (
+    // <div>
+    //   <Intro />
+    // </div>
+    <Main />
+  );
+};
+
+export default App;
---
+++ Resource/CULISO/front/src/Intro.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export const Intro = () => {
+  return (
+    <div className="intro">
+      <div className="removebg-preview-wrapper"></div>
+    </div>
+  );
+};
