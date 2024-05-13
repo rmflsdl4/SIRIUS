78ea86f27f9104c8b61be88fe7805cbf3256919b
SeongBeen <142138080+psb3505@users.noreply.github.com>
2024-05-13 17:35:43 +0900

    서버 연동 성공

--- Resource/CULISO/server.js
+++ Resource/CULISO/server.js
@@ -15,3 +15,13 @@ app.post("/signUp", (req) => {
   console.log("유저에게 받은 회원가입 데이터");
   console.log(data['id']);
 });
+
+
+// Web 영역
+app.post("/viewDetailsModalSendData", (req, res) => {
+    const { modalSendData } = req.body;
+    console.log("modalSendData : " + modalSendData);
+
+    // JSON 형식으로 데이터를 반환
+    res.json("Received modalSendData: " + modalSendData);
+});
