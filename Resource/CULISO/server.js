8ce299e57c6dfa53ba49130b48d3779a02333f1b
SeongBeen <142138080+psb3505@users.noreply.github.com>
2024-05-13 22:46:04 +0900

    회원 관리 메인 화면 테이블 값 불러오기 완료

--- Resource/CULISO/server.js
+++ Resource/CULISO/server.js
@@ -7,6 +7,10 @@ const cors = require("cors");
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+const database = require('./database.js');
+
+// 데이터베이스 연결
+database.Connect();
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
@@ -18,10 +22,75 @@ app.post("/signUp", (req) => {
 
 
 // Web 영역
-app.post("/viewDetailsModalSendData", (req, res) => {
-    const { modalSendData } = req.body;
-    console.log("modalSendData : " + modalSendData);
+// 회원 관리 초기 데이터
+app.post("/adminMainMgrInitData", async (req, res) => {
+    const adminID = 'admin';
+
+    const query = `select userID, userName, userNickName, createDate
+                    from user
+                    where adminID = ?`;
+
+    const values = [adminID];
+
+    const result = await database.Query(query, values);
+    
+    // console.log(result);
 
     // JSON 형식으로 데이터를 반환
-    res.json("Received modalSendData: " + modalSendData);
+    res.json(result);
+});
+
+// 게시판 관리 초기 데이터
+app.post("/boardMgrInitData", async (req, res) => {
+    const adminID = 'admin';
+
+    const query = `select userID, userName, userNickName, createDate
+                    from user
+                    where adminID = ?`;
+
+    const values = [adminID];
+
+    const result = await database.Query(query, values);
+    
+    console.log(result);
+
+    // JSON 형식으로 데이터를 반환
+    res.json(result);
+});
+
+
+// adminMain 상세보기 데이터 select
+app.post("/adminMainViewDetails", (req, res) => {
+    const { modalSendData } = req.body;
+    console.log("adminMain 상세보기 데이터 : " + modalSendData);
+
+    // JSON 형식으로 데이터를 반환
+    res.json("adminMain 상세보기 데이터: " + modalSendData);
+});
+
+// adminMain 프로필카드 데이터 select
+app.post("/profileViewDetails", (req, res) => {
+    const { modalSendData } = req.body;
+    console.log("adminMain 프로필카드 데이터 : " + modalSendData);
+
+    // JSON 형식으로 데이터를 반환
+    res.json("adminMain 프로필카드 데이터: " + modalSendData);
+});
+
+// boardMgr 상세보기 데이터 select
+app.post("/boardMgrViewDetails", (req, res) => {
+    const { modalSendData } = req.body;
+    console.log("boardMgr 상세보기 데이터 : " + modalSendData);
+
+    // JSON 형식으로 데이터를 반환
+    res.json("boardMgr 상세보기 데이터: " + modalSendData);
+});
+
+// requestMgr 상세보기 데이터 select
+app.post("/requestMgrViewDetails", (req, res) => {
+    const { modalSendData } = req.body;
+    console.log("requestMgr 상세보기 데이터 : " + modalSendData);
+
+    // JSON 형식으로 데이터를 반환
+    res.json("requestMgr 상세보기 데이터: " + modalSendData);
 });
