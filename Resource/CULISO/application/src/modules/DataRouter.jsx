5dfeab248cd1111a04a62eea8ee2846d19af1004
rmflsdl4 <rmflsdl4@gmail.com>
2024-05-26 14:07:24 +0900

    443

--- Resource/CULISO/application/src/modules/DataRouter.jsx
+++ Resource/CULISO/application/src/modules/DataRouter.jsx
@@ -3,7 +3,7 @@ import moment from 'moment';
 
 
 const host = "https://13.209.80.79"; // 추후에 Let's Encrypt 와 같은 사이트에서 SSL 발급받아서 https로 접근해서 보안을 강화해야 함
-const port = "8001";
+const port = "443";
 const url = host + ":" + port + "/";
 const cookies = new Cookies();
 
