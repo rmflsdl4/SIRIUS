08e4c7ff039b8d404396398304863b9b41a6ca9e
rmflsdl4 <rmflsdl4@gmail.com>
2024-07-19 00:26:30 +0900

    리소스 삭제

--- Resource/CULISO/application/android/app/src/main/java/com/example/sirius/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.example.sirius;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- Resource/CULISO/web/src/modules/GetIcon.js
+++
@@ -1,4 +0,0 @@
-// 이미지 받아오기
-export function GetIcon(iconName) {
-    return process.env.PUBLIC_URL + "/" + iconName;
-}
