04fcbc6a8a90601f912ec39b25c8a7df914ad582
rmflsdl4 <rmflsdl4@gmail.com>
2024-07-19 00:57:12 +0900

    리소스 삭제

--- Resource/CULISO/application/src/modules/GetIcon.js
+++
@@ -1,9 +0,0 @@
-// 이미지 받아오기
-export default function GetIcon(iconName) {
-  // 업로드된 이미지가 data URL 형태인 경우
-  if (typeof iconName === "string" && iconName.startsWith("data:image")) {
-    return iconName;
-  }
-  // 로컬 경로의 이미지인 경우
-  return process.env.PUBLIC_URL + "/" + iconName;
-}
