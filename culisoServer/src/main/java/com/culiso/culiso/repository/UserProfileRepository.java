a3144155fdcb3f315c454d3c1af717cbb7a3df43
SeongBeen <142138080+psb3505@users.noreply.github.com>
2024-08-09 23:21:12 +0900

    게시판 메뉴, 유저 프로필 레파지토리 설정

---
+++ culisoServer/src/main/java/com/culiso/culiso/repository/UserProfileRepository.java
@@ -0,0 +1,17 @@
+package com.culiso.culiso.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.culiso.culiso.dto.UserProfileDTO;
+import com.culiso.culiso.entity.UserEntity;
+
+public interface UserProfileRepository extends JpaRepository<UserEntity, String> {
+    @Query("SELECT UserProfileDTO(user.user_id, user.user_name, user.user_nick, user.create_date, user.profile_url) FROM UserEntity user WHERE user.delete_flag = false and user.user_id = 'user1'")
+    // Optional<Long> loginHandler(@Param("id") String id);
+    List<UserProfileDTO> userProfileHandler();
+}
