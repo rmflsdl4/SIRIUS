65349804c0dc6fb41e3eb16e44789bcaad29b8db
SeongBeen <142138080+psb3505@users.noreply.github.com>
2024-08-15 00:23:34 +0900

    게시판 데이터 불러오기 성공

---
+++ culisoServer/src/main/java/com/culiso/culiso/service/PostContentsService.java
@@ -0,0 +1,30 @@
+package com.culiso.culiso.service;
+
+import java.util.List;
+
+import javax.print.DocFlavor.STRING;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.culiso.culiso.dto.CommentDTO;
+import com.culiso.culiso.dto.FileDTO;
+import com.culiso.culiso.dto.PostContentsDTO;
+import com.culiso.culiso.dto.CombinedContentsResponseDTO;
+import com.culiso.culiso.repository.PostContentsRepository;
+
+@Service
+public class PostContentsService {
+
+    @Autowired
+    private PostContentsRepository postContentsRepository;
+
+    public CombinedContentsResponseDTO postContents(int contents_num, String sessionUserID) {
+        List<PostContentsDTO> contentsResult = postContentsRepository.getPostContents(contents_num);
+        List<CommentDTO> commentResult = postContentsRepository.getComments(contents_num);
+        List<FileDTO> fileResult = postContentsRepository.getFiles(contents_num);
+        int contentsRecommendResult = postContentsRepository.countUserRecommend(contents_num);
+
+        return new CombinedContentsResponseDTO(contentsResult, commentResult, fileResult, contentsRecommendResult, sessionUserID);
+    }
+}
