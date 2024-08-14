7ac0b4cecbb8555d6114dc84a36b9e1a3dd1e7b1
SeongBeen <142138080+psb3505@users.noreply.github.com>
2024-08-15 00:23:11 +0900

    게시판 데이터 불러오기 성공

---
+++ culisoServer/src/main/java/com/culiso/culiso/repository/PostContentsRepository.java
@@ -0,0 +1,46 @@
+package com.culiso.culiso.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.culiso.culiso.dto.CommentDTO;
+import com.culiso.culiso.dto.FileDTO;
+import com.culiso.culiso.dto.PostContentsDTO;
+import com.culiso.culiso.entity.ContentsEntity;
+
+public interface PostContentsRepository extends JpaRepository<ContentsEntity, Integer> {
+    @Query("SELECT new com.culiso.culiso.dto.PostContentsDTO(" +
+            "u.user_id, u.user_name, u.user_nick, u.create_date, u.profile_url, " +
+            "c.contents_num, c.contents_title, c.content, c.recommend, c.contents_date, c.views, " +
+            "b.board_id, b.board_name) " +
+            "FROM ContentsEntity c " +
+            "INNER JOIN UserEntity u ON c.user_id = u.user_id " +
+            "INNER JOIN BoardEntity b ON c.board_id = b.board_id " +
+            "WHERE c.contents_num = :contents_num")
+            List<PostContentsDTO> getPostContents(@Param("contents_num") int contents_num);
+
+
+    @Query("SELECT new com.culiso.culiso.dto.CommentDTO(" +
+            "c.comment_num, c.comment_content, c.comment_date, " +
+            "u.user_id, u.user_name, u.user_nick, u.profile_url) " +
+            "FROM CommentEntity c " +
+            "INNER JOIN UserEntity u ON c.user_id = u.user_id " +
+            "WHERE c.contents_num = :contents_num")
+    List<CommentDTO> getComments(@Param("contents_num") int contents_num);
+
+
+    @Query("SELECT new com.culiso.culiso.dto.FileDTO(" +
+            "f.file_upload_num, f.file_url, f.file_name) " +
+            "FROM FileEntity f " +
+            "WHERE f.contents_num = :contents_num")
+    List<FileDTO> getFiles(@Param("contents_num") int contents_num);
+
+
+    @Query("SELECT COUNT(cr) FROM ContentsRecommendEntity cr " +
+           "WHERE cr.id.user_id = 'user1' AND cr.id.contents_num = :contents_num")
+    int countUserRecommend(@Param("contents_num") int contents_num);
+    // int countUserRecommend(@Param("user_id") String user_id, @Param("contents_num") int contents_num);
+}
