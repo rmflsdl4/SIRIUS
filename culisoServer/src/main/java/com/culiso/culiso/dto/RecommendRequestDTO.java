885fbd688ace21ed989e2a47bcfbcf7bb0f38e74
SeongBeen <142138080+psb3505@users.noreply.github.com>
2024-08-16 23:20:42 +0900

    게시글 기능 구현 (이미지 파일 저장 및 보여주는 기능 추가해야 함)

---
+++ culisoServer/src/main/java/com/culiso/culiso/dto/RecommendRequestDTO.java
@@ -0,0 +1,13 @@
+package com.culiso.culiso.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor 
+public class RecommendRequestDTO {
+    private int contents_num;
+    private int check;
+}
