79ae9c6454d9445929894f18e05ca2f5d7d07fcb
SeongBeen <142138080+psb3505@users.noreply.github.com>
2024-08-24 01:47:49 +0900

    게시글 생성 및 파일 업로드 완료

---
+++ culisoServer/src/main/java/com/culiso/culiso/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.culiso.culiso.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // "/images/**" 경로로 요청된 리소스는 "classpath:/static/images/" 디렉토리에서 찾는다.
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/images/")
+                .setCachePeriod(100); // 캐시 기간을 초 단위로 설정
+    }
+}
