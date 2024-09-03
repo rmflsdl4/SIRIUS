786c9c1fa79f2d6141f78497aa0fec92140b3cb0
SeongBeen <142138080+psb3505@users.noreply.github.com>
2024-09-03 21:16:20 +0900

    수정

--- culisoServer/src/main/java/com/culiso/culiso/repository/BoardRepository.java
+++ culisoServer/src/main/java/com/culiso/culiso/repository/BoardRepository.java
@@ -11,4 +11,12 @@ import com.culiso.culiso.entity.BoardEntity;
 public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
     @Query("SELECT new com.culiso.culiso.dto.BoardMenuDTO(board.board_id, board.board_name) FROM BoardEntity board WHERE board.board_read = 'A' OR board.board_read = 'U'")
     List<BoardMenuDTO> boardMenuHandler();
+
+    @Query("SELECT new com.culiso.culiso.dto.BoardMenuDTO(board.board_id, board.board_name) " +
+       "FROM BoardEntity board " +
+       "WHERE board.board_name != '모아보기' " +
+       "AND (board.board_write = 'A' OR board.board_write = 'U') " +
+       "ORDER BY board.board_id")
+    List<BoardMenuDTO> checkBoardHandler();
+
 } 
