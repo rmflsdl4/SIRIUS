7e056587fa7efa42860a0398fcfaaf46b626456e
SeongBeen <142138080+psb3505@users.noreply.github.com>
2024-10-08 23:15:05 +0900

    블루투스 목록 선택 기능 추가

--- CulisoApp/modules/Bluetooth.jsx
+++ CulisoApp/modules/Bluetooth.jsx
@@ -1,42 +1,31 @@
 import { BleManager } from 'react-native-ble-plx';
 import { BluetoothCheck } from "../modules/PermissionUtil";
-import { useState } from 'react';
 
 const manager = new BleManager();
 
-export const BluetoothConnect = async () => {
-    const result = await BluetoothCheck();
-    const state = await manager.state();
+export const BluetoothConnect = async (onDeviceDiscovered) => {
+    const result = await BluetoothCheck(); // 블루투스 권한 체크
+    const state = await manager.state(); // 블루투스 상태 확인
     console.log("권한상태: ", result);
     console.log("블루: ", state);
+
     if (result === 'granted') {
-        return new Promise((resolve, reject) => {
-            const targetDeviceId = 'C6:23:48:AD:7F:C7';
+        manager.startDeviceScan(null, null, (error, device) => {
+            if (error) {
+                console.log("스캔 에러: ", error);
+                return;
+            }
 
-            manager.startDeviceScan(null, null, (error, device) => {
-                console.log("발견된 기기: ", device.id);
-
-                if (error) {
-                    console.log("스캔 에러: ", error);
-                    reject(error);
-                    return;
-                }
-
-                if (device && device.id === targetDeviceId) {
-                    console.log("찾은 기기: ", device.id);
-                    manager.stopDeviceScan();
-                    resolve(device);
-                }
-            });
-
-            // 10초 동안 못 찾으면 중지하고 널값 반환
-            setTimeout(() => {
-                manager.stopDeviceScan();
-                console.log("타임 아웃");
-                resolve(null);
-            }, 10000);
+            if (device && onDeviceDiscovered) {
+                console.log("발견된 기기: ", device.id, device.name);
+                onDeviceDiscovered(device);  // 기기를 발견하면 콜백 함수 호출
+            }
         });
-    } else {
-        return null;
+
+        // 10초 뒤에 탐색 종료
+        setTimeout(() => {
+            manager.stopDeviceScan();
+            console.log("타임 아웃, 스캔 종료");
+        }, 10000);
     }
-}
+};
